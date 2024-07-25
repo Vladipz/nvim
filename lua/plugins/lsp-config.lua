724a91cb5f0d4a87efe3593f27a6163bd1c46dde
Vladyslav Danilchuk <vladdanilchuk9@gmail.com>
2024-07-25 21:31:54 +0300

    setup emmet ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -126,8 +126,21 @@ return {
 
       lspconfig.lua_ls.setup(default_config)
       lspconfig.html.setup(default_config)
-      lspconfig.emmet_language_server.setup(default_config)
+      -- lspconfig.emmet_language_server.setup(default_config)
       lspconfig.cssls.setup(default_config)
+      lspconfig.emmet_ls.setup({
+        -- on_attach = on_attach,
+        capabilities = capabilities,
+        filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
+        init_options = {
+          html = {
+            options = {
+              -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+              ["bem.enabled"] = true,
+            },
+          },
+        }
+      })
       lspconfig.css_variables.setup(default_config)
 
       lspconfig.pyright.setup({
