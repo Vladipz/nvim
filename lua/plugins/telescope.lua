c9e5937f6b42f9262c695f082820a4707badb7ca
Vladyslav Danilchuk <vladdanilchuk9@gmail.com>
2024-07-26 21:33:49 +0300

    fix

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -20,7 +20,7 @@ return { {
     local builtin = require("telescope.builtin")
     vim.keymap.set('n', '<C-p>', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, {})
+    -- vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, {})
 
     --vim.keymap.set('n', '<leader>gi', builtin.lsp_implementations(), {})
   end
