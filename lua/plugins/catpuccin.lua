87aa8e80a6ef22158a6a8b74b60d73c07a03d139
Vladyslav Danilchuk <vladdanilchuk9@gmail.com>
2024-07-31 19:25:22 +0300

    change colorscheme

--- lua/plugins/catpuccin.lua
+++ lua/plugins/catpuccin.lua
@@ -1,10 +1,19 @@
-return  {
+return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
   lazy = false,
   config = function()
-    vim.cmd.colorschem "catppuccin"
+    require("catppuccin").setup {
+      custom_highlights = function(C)
+        return {
+          ["@property.class"] = { fg = C.yellow },
+          ["@property.css"] = { fg = C.blue },
+          ["@property.scss"] = { fg = C.blue },
+          ["@variable"] = { fg = C.blue },
+        }
+      end
+    }
+    vim.cmd.colorschem "catppuccin-macchiato"
   end
 }
-
--- lua/plugins/multiple-cursors.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- "terryma/vim-multiple-cursors",
-  -- config = function()
-  --   vim.api.nvim_set_keymap("n", "<C-n>", "<Plug>(multicursor-next)", {})
-  --   vim.api.nvim_set_keymap("n", "<C-p>", "<Plug>(multicursor-prev)", {})
-  --   vim.api.nvim_set_keymap("n", "<C-x>", "<Plug>(multicursor-skip)", {})
-  --   vim.api.nvim_set_keymap("n", "<C-a>", "<Plug>(multicursor-all)", {})
-  -- end
-}
