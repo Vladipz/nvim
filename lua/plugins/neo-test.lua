c05170776a9235ac2226d1b8a049e1ec5d3dba6f
Vladyslav Danilchuk <vladdanilchuk9@gmail.com>
2024-08-13 18:21:05 +0300

    fix neotest test explorer width

--- lua/plugins/neo-test.lua
+++ lua/plugins/neo-test.lua
@@ -12,7 +12,17 @@ return {
     { "<leader>tr", function() require("neotest").run.run({ strategy = "dap" }) end, desc = "[T]est [R]un" },
     { "<leader>tR", function() require("neotest").run.run(vim.fn.expand("%")) end,   desc = "[T]est [R]UN (all)" },
     { "<leader>to", function() require("neotest").output.open({ enter = true }) end, desc = "[T]est [O]utput" },
-    { "<leader>te", function() require("neotest").summary.toggle() end,              desc = "[T]est [E]xplorer" },
+    {
+      "<leader>te",
+      function()
+        local summary_window = require("neotest").summary
+        summary_window.toggle()
+        -- Задаємо розміри вікна
+        local win = vim.api.nvim_get_current_win()
+        vim.api.nvim_win_set_width(win, 40)  -- ширина вікна
+      end,
+      desc = "[T]est [E]xplorer"
+    },
     { "<leader>ts", function() require("neotest").output_panel.toggle() end,         desc = "[T]est [S]ummary" },
     { "<leader>td", function() require("neotest").run.run({ strategy = "dap" }) end, desc = "[T]est [D]ebug" },
   },
@@ -30,7 +40,9 @@ return {
         } }),
         require("neotest-python")({ runner = "pytest", python = ".venv/Scripts/python.exe", }),
       },
-      output = { open_on_run = true },
+      output = {
+        open_on_run = true
+      },
       quickfix = {
         enabled = false,
         open = false,
